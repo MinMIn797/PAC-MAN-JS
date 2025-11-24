4fc5a9f53189c3e846068377da5a8e09a194c301
MinMIn797 <nikatorosenko@gmail.com>
2025-11-24 19:30:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
-const pacmanFrames = document.getElementById("animations");
-const ghostFrames = document.getElementById("ghost");
+const pacmanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts");
 
 function createRect(x, y, width, height, color) {
     canvasContext.fillStyle = color;
@@ -208,3 +208,4 @@ createNewPacman();
 createGhosts();
 
 gameLoop();
+
